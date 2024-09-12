a6e241d40a1118f9b15be1bc0c68f30e802f425b
shemil01 <shemilmk1033@gmail.com>
2024-09-12 11:02:51 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import SearchInMob from "./components/search/SearchInMob";
 import MainPage from "./pages/mainPage/MainPage";
 
 function App() {
   return (
     <div>
-      {/* <MainPage /> */}
-      <SearchInMob/>
+      <MainPage />
     </div>
   );
 }
